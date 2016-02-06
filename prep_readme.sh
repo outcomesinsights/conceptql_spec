91f28d3ace1dec30f41f4ea91b3f538fd280a253
Ryan Duryea <aguynamedryan@gmail.com>
2016-02-05 16:11:47 -0800

    Add --clean to prep_readme.sh

--- prep_readme.sh
+++ prep_readme.sh
@@ -1,3 +1,8 @@
+if [[ $1 == "--clean" ]]; then
+  echo "Cleaning house"
+  rm -rf .????????* README
+fi
+bundle exec sequelizer config
 bundle exec conceptql knit README.md.cql
 doctoc --notitle README.md
 mdl README.md
