03ab1e20f91d4b6bb3b0483b5eebc18b39c082f0
Ryan Duryea <aguynamedryan@gmail.com>
2016-03-08 10:26:58 -0800

    prep: don't delete .gitignore, run under bash

--- prep_readme.sh
+++ prep_readme.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 if [[ $1 == "--clean" ]]; then
   echo "Cleaning house"
-  rm -rf .????????* README
+  rm -rf .??????????????* README
 fi
+
 bundle exec sequelizer config
 bundle exec conceptql knit README.md.cql
 doctoc --bitbucket --notitle README.md
